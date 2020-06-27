95c7fdc4e77c8a970204dee06f4e0c631fefdbcf
Jico <jico@velocitylight.net>
2020-06-27 19:04:38 +0800

    first commit

---
+++ components/MapBox/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div
+    class="mapBox"
+    :style="'background-color: rgba(' + bgc + ');'"
+    @click="handleClick()"
+  >
+    <div class="mapBoxName">
+      {{ data.name }}
+    </div>
+    <div class="mapBoxPrice">$ {{ data.price }}</div>
+    {{ level }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MapBox',
+  props: { data: Object, buy: Function },
+  data() {
+    return {
+      level: 1,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.buy();
+      this.level++;
+    },
+  },
+  computed: {
+    bgc: function () {
+      const randomV = (num) => Math.round(Math.random() * num);
+      const newColor = [randomV(250), randomV(160), randomV(25)];
+      return newColor;
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.mapBox {
+  width: 150px;
+  height: 150px;
+  padding: 10px;
+  background-color: #369;
+  color: white;
+  cursor: pointer;
+  user-select: none;
+  &Name {
+    font-size: 24px;
+  }
+  &Price {
+    font-size: 16px;
+  }
+}
+</style>
---
+++ screens/Main.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <h1>Hello! {{ playerName }} $ {{ yourMoney }}</h1>
+    <div class="map">
+      <div v-for="(item, index) in places" :key="index">
+        <MapBox :data="item" :buy="handleBuy" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MapBox from '../components/MapBox/';
+
+export default {
+  name: 'Main',
+  components: {
+    MapBox,
+  },
+  data() {
+    return {
+      yourMoney: 100000,
+      places: [
+        {
+          name: 'New York',
+          price: 3000,
+        },
+        {
+          name: 'Taipei',
+          price: 1000,
+        },
+        {
+          name: 'Tokyo',
+          price: 1500,
+        },
+        {
+          name: 'Osaka',
+          price: 1300,
+        },
+      ],
+      playerName: 'Jico',
+    };
+  },
+  methods: {
+    handleBuy() {
+      this.yourMoney -= 1000;
+    },
+  },
+};
+</script>
+
+<style>
+.map {
+  width: 800px;
+  height: 500px;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+}
+</style>
